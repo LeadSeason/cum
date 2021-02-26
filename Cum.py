2d739196e6be1c94a2ac1232edd0e8237fa09ec7
LeadSeason <pauli.tiittanen113@gmail.com>
2021-02-26 23:58:37 +0200

    a

--- Cum.py
+++ Cum.py
@@ -11,17 +11,11 @@ def f():
         print("X:", x)
         y = random.randint(0, 1080)
         print("Y:", y)
-        pyautogui.moveTo(x, y, duration = 0)
+        pyautogui.moveTo(x, y, duration=0)
 
 
-def main():
+for i in range(25):
+    Thread(target=f)
 
-    for i in range(25):
-        Thread(target=f)
-
-    while True:
-        pass
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    pass
